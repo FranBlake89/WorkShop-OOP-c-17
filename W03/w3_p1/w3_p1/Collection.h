028b74587eaf4b39d60d139e1696b4a1ee4cb2f1
Francisco Castillo <73005797+FranBlake89@users.noreply.github.com>
2023-02-01 21:32:49 -0500

    v2

--- W03/w3_p1/w3_p1/Collection.h
+++ W03/w3_p1/w3_p1/Collection.h
@@ -12,21 +12,61 @@ Section:	NDD
 #include <string>
 //This module represents a family of collections of elements of any data type 
 //(for example, collection of ints, or collection of Pairs, etc.).
+namespace sdds {
 
-template <typename T, size_t CAPACITY>
+	template <typename T, size_t CAPACITY>
 
-class Collection
-{
-	size_t m_size{};
-	T m_items[CAPACITY]{};
-	T dummyObj{};
+	class Collection
+	{
+		size_t m_size{};
+		T m_items[CAPACITY]{};
+		inline static T dummyObj{};
 
-public:
-	Collection() : size(0) {};
-	int size() { return m_size; }
-	virtual ~Collection() {};
-	void display(std::ostream& os = std::cout) const;
+	public:
+		Collection() : m_size(0) {};
+		virtual ~Collection() {    };
+		int size() { return m_size; }
+		void display(std::ostream& os = std::cout) const {
+
+			os << "----------------------" << std::endl;
+			os << "| Collection Content |" << std::endl;
+			os << "----------------------" << std::endl;
+
+			for (size_t i = 0; i < CAPACITY; i++)
+			{
+				os  << m_items[i] << std::endl;
+			}
 
 
-};
+			os << "----------------------" << std::endl;
+		};
+		bool add(const T& item) {		
+			bool was_Added = false;
+		
+			if (m_size < CAPACITY) {
 
+				m_items[m_size] = item;
+				m_size++;
+				was_Added = true;
+			}
+
+			return was_Added;
+		};
+		T operator[] (int index) {
+			
+			T copy = dummyObj;
+
+			if (index > CAPACITY) {
+				
+			}
+			else {
+				copy = m_items[index];
+			}	
+			return copy;
+		};
+
+
+	};
+
+
+}
--- W03/w3_p1/w3_p1/Pair.h
+++ W03/w3_p1/w3_p1/Pair.h
@@ -12,18 +12,38 @@ Section:	NDD
 #include <iostream>
 #include <string>
 //This module represents a value-key pair (like the words and their definition in a dictionary).
+namespace sdds {
+
+
 class Pair
 {
 	std::string m_key{};
 	std::string m_value{};
 
 public:
+	Pair() {};
 	const std::string& getKey() { return m_key; }
 	const std::string& getValue() { return m_value; }
 	Pair(const std::string& key, const std::string& value) : m_key{ key }, m_value{ value } {};
 	//Two objects of type Pair are considered to be equal if they have the same key.
 	// In the reflection justify why it was necessary to add each one of the members you have decided to add.
+	
+	Pair& operator==(const Pair& OBJ) {
 
+	}
+	
+	//isEmpty
+void display(std::ostream& os) const {
+		os << this->m_value << std::endl;
+
+	};
 
 };
 
+std::ostream& operator<<(std::ostream& os, const Pair& obj) {
+//eturn obj.display(os);
+	obj.display(os);
+	return os;
+};
+
+}
